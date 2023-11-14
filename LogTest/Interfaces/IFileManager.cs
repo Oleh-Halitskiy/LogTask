a3b892416c04c83293549d536bdd9038558849ed
Ex0oleg <olegexo@gmail.com>
2023-11-14 20:45:23 +0200

    move files to according folders and add file manager

---
+++ LogTest/Interfaces/IFileManager.cs
@@ -0,0 +1,8 @@
+﻿namespace LogTest.Interfaces
+{
+    public interface IFileManager
+    {
+        void CreateFile(string directoryPath, string fileName);
+        void WriteToFile(string path, string content);
+    }
+}
---
+++ LogTest/Utils/FileManager.cs
@@ -0,0 +1,31 @@
+﻿using LogTest.Interfaces;
+using System.IO;
+
+namespace LogTest.Utils
+{
+    public class FileManager : IFileManager
+    {
+        public void CreateFile(string directoryPath, string fileName)
+        {
+            string filePath = Path.Combine(directoryPath, fileName);
+
+            if (!Directory.Exists(directoryPath))
+            {
+                Directory.CreateDirectory(directoryPath);
+            }
+            File.Create(filePath);
+        }
+
+        public void WriteToFile(string path, string content)
+        {
+            if (File.Exists(path))
+            {
+                File.WriteAllText(path, content);
+            }
+            else
+            {
+                throw new DirectoryNotFoundException();
+            }
+        }
+    }
+}
