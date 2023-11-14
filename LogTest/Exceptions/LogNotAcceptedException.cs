fbe2950d9b99d26f8740d38f43c77caba6974035
Ex0oleg <olegexo@gmail.com>
2023-11-14 22:38:49 +0200

    Added exceptions class

---
+++ LogTest/Exceptions/LogNotAcceptedException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace LogTest.Exceptions
+{
+    internal class LogNotAcceptedException : Exception
+    {
+        public LogNotAcceptedException()
+        {
+        }
+
+        public LogNotAcceptedException(string message) : base(message)
+        {
+        }
+
+        public LogNotAcceptedException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+}
