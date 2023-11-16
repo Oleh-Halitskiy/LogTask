f1030e7c681c8f614e7709834460f1350080e52d
Ex0oleg <olegexo@gmail.com>
2023-11-17 00:45:19 +0200

    added unit tests for filemanager and logline

---
+++ LogComponent.Tests/FileManagerTests.cs
@@ -0,0 +1,64 @@
+﻿using LogTest.Utils;
+using System.IO;
+using Xunit;
+
+namespace LogComponent.Tests
+{
+    public class FileManagerTests
+    {
+        [Fact]
+        public void CreateFile_ShouldCreateFile()
+        {
+            var fileManager = new FileManager();
+            var directoryPath = Path.Combine(Path.GetTempPath(), "TestDirectory");
+            var fileName = "TestFile.txt";
+            var filePath = Path.Combine(directoryPath, fileName);
+
+            try
+            {
+                fileManager.CreateFile(directoryPath, fileName);
+
+                Assert.True(File.Exists(filePath));
+            }
+            finally
+            {
+                Directory.Delete(directoryPath, true);
+            }
+        }
+
+        [Fact]
+        public void WriteToFile_ShouldWriteContentToFile()
+        {
+            var fileManager = new FileManager();
+            var directoryPath = Path.Combine(Path.GetTempPath(), "TestDirectory");
+            var fileName = "TestFile.txt";
+            var filePath = Path.Combine(directoryPath, fileName);
+            var content = "Hello, world!";
+
+            try
+            {
+                fileManager.CreateFile(directoryPath, fileName);
+                fileManager.WriteToFile(filePath, content);
+
+                var fileContent = File.ReadAllText(filePath);
+                Assert.Contains(content, fileContent);
+            }
+            finally
+            {
+                Directory.Delete(directoryPath, true);
+            }
+        }
+
+        [Fact]
+        public void WriteToFile_ShouldThrowDirectoryNotFoundException()
+        {
+            var fileManager = new FileManager();
+            var nonExistentPath = Path.Combine(Path.GetTempPath(), "NonExistentDirectory");
+            var fileName = "NonExistentFile.txt";
+            var filePath = Path.Combine(nonExistentPath, fileName);
+            var content = "Hello, world!";
+
+            Assert.Throws<DirectoryNotFoundException>(() => fileManager.WriteToFile(filePath, content));
+        }
+    }
+}
---
+++ LogComponent.Tests/LogLineTests.cs
@@ -0,0 +1,43 @@
+﻿using LogTest.Core;
+using System;
+using Xunit;
+
+namespace LogComponent.Tests
+{
+    public class LogLineTests
+    {
+        [Fact]
+        public void DefaultConstructor_ShouldInitializeProperties()
+        {
+            var logLine = new LogLine();
+
+            Assert.Equal("", logLine.Text);
+            Assert.Equal(DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss:fff"), logLine.TimeStamp.ToString("yyyy-MM-dd HH:mm:ss:fff"));
+        }
+
+        [Fact]
+        public void ParameterizedConstructor_ShouldSetProperties()
+        {
+            var text = "Test log entry";
+            var timeStamp = new DateTime(2023, 1, 1, 12, 0, 0);
+
+            var logLine = new LogLine(text, timeStamp);
+
+            Assert.Equal(text, logLine.Text);
+            Assert.Equal(timeStamp.ToString("yyyy-MM-dd HH:mm:ss:fff"), logLine.TimeStamp.ToString("yyyy-MM-dd HH:mm:ss:fff"));
+        }
+
+        [Fact]
+        public void ToString_ShouldReturnFormattedString()
+        {
+            var text = "Test log entry";
+            var timeStamp = new DateTime(2023, 1, 1, 12, 0, 0);
+            var logLine = new LogLine(text, timeStamp);
+
+            var result = logLine.ToString();
+
+            var expectedString = $"{timeStamp.ToString("yyyy-MM-dd HH:mm:ss:fff")}: {text}";
+            Assert.Equal(expectedString, result);
+        }
+    }
+}
